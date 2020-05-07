3df16f64781ad84ff1b0a7a3bce47ee6900eff5e
Matthew Agler <matthew.agler@uni-jena.de>
2020-05-07 11:46:47 +0200

    This is is a major set of changes intended to fix some previous problems with the quality of the reported oligos and to get better quantificaiton of how good each oligo is. I basically changed the Blasting function to now default to word length 7, 25 percent ID and really high e-value cutoff, which means relatively poor alignments of down to 7 bases are returned. This is so that we can see everywhere each kmer is likely to prime. In the output, besides improving the quality of the graph and going to outputting a single table of all "kmers", we now report counts of not just total hits but also long alignments and 3' alignments. This is because in some cases there are no good candidates using the total hit count, but how well the oligos align in the 3' end (where the polymerase sits) is anyway more important. So that script was almost completely revised. In the process the imported packages also changed.

--- R/Blast_Oligo_Candidates_Func.r
+++ R/Blast_Oligo_Candidates_Func.r
@@ -1,10 +1,12 @@
 #' Blast Candidate Oligomers
 #'
-#' This function will blast candidate oligomers (from a nontarget organism) against a database of target organisms and returns a parsable format.
+#' This function will blast candidate oligomers (from a nontarget organism) against a database of target organisms and return a parsable format. The default parameters are set to identify quite poor alignments with the goal of identifying as well as possible any potential matches.
 #' @param blastn_path Path to blastn. Defaults to "blastn". If your blast copy is somewhere else, please point to it here.
 #' @param blastdb Filepath for the blast databse. No Default
 #' @param candidates Filepath for the candidate oligomers file. No Default.
 #' @param outfile Filepath for the output file name, which will contain the blast results. Defaults to "Candidates_vs_UNITE.out"
+#' @param percid Minimum percent identity to use for the blast search (default is 25)
+#' @param wordsize Word size to use in the blast search (default is 7)
 #' @keywords nontarget
 #' @keywords length
 #' @keywords outfile
@@ -12,12 +14,13 @@
 #' @examples
 #' blast_candidate_oligos(blastdb="./Unite_s_011217/UNITE_s_011217")
 
-OS <- Sys.info()['sysname']
 
-blast_candidate_oligos <- function(blastn_path = "blastn", blastdb, candidates, outfile="Candidate_vs_UNITE.out"){
+blast_candidate_oligos <- function(blastn_path = "blastn", blastdb, candidates, outfile="Candidate_vs_UNITE.out", percid=25, wordsize=7){
+
+  OS <- Sys.info()['sysname']
 
 	# create blast command function. The parameters are currently not adjustable, but will be printed
-  cmd <- paste(blastn_path, " -db ", blastdb ," -query ", candidates ," -out ", outfile," -outfmt \"7 nident std\" -perc_identity 50 -word_size 12 -dust no -evalue 1000 -max_target_seqs 1000 ", sep = "")
+  cmd <- paste(blastn_path, " -db ", blastdb ," -query ", candidates ," -out ", outfile," -outfmt \"7 nident std qseq sseq\" -perc_identity ",percid," -word_size ",wordsize," -dust no -evalue 100000 -max_target_seqs 1000 ", sep = "")
   paste("Running Blast command: ",cmd, sep = "")
 
   # Run the blast command. This is OS-specific
--- R/Make_Output_Func.r
+++ R/Make_Output_Func.r
@@ -1,6 +1,6 @@
 #' Check Oligomers and Potential Primer Pairs
 #'
-#' This function takes output from get_candidate_oligos() and blast_candidate_oligos(). It checks each candidate for how many times it hit the target organisms, then sorts potential pairs to minimize the number of hits. The pairs in the output table can then be manually checked for ideal primer characteristics, etc.
+#' This function takes output from get_candidate_oligos() and blast_candidate_oligos(). It checks each candidate for how many times they hits the target organisms (in the example fungi in the Unite ITS database), how many times the hits align along >90% of the length of the primer, and how many hits align at the 3' end of the primer (alignments are counted if they end within 3 bases of the end of hte primer. This type of alignment is the most serious since the polymerase binds there). It outputs a graph (in pdf format) with each of the three counts as well as the same data with the primer sequences in a tab-delimited table. Good candidates can be checked subsequently for the ideal primer characteristics, etc.
 #' @param candidates Filepath for the candidate oligomers file. No Default.
 #' @param blastoutput Filepath for the output from blasting the candidates against the target database. No Default.
 #' @param outputgraph Filepath for the output graph, which will show how many times each oligo hits target sequences. Defaults to "Oligos_Plot.pdf".
@@ -9,54 +9,103 @@
 #' @keywords blastoutput
 #' @keywords outputgraph
 #' @keywords outputfile
-#' @import dplyr
 #' @import tidyr
+#' @import ggpubr
+#' @import ggplot2
 #' @export
 #' @examples
 #' check_candidate_oligos(candidates="Candidate_oligos.fasta", blastoutput="Candidate_vs_UNITE.out")
 
 check_candidate_oligos <- function(candidates, blastoutput, outputgraph="Oligos_Plot.pdf", outputfile="OligoPairs_Table.txt"){
 
-	# Get the query file into a data frame
-	kmers <- grep('kmer', readLines(candidates), value=T)
-	seqs <- grep('kmer', readLines(candidates), value=T, invert=T)
-	kmers <-as.data.frame(kmers)
-	seqs <- as.data.frame(seqs)
-	kmers <- cbind(kmers,seqs)
+  # Get the query file into a data frame
+  kmers <- grep('kmer', readLines(candidates), value=T)
+  seqs <- grep('kmer', readLines(candidates), value=T, invert=T)
+  kmers <-as.data.frame(kmers)
+  seqs <- as.data.frame(seqs)
+  kmers <- cbind(kmers,seqs)
 
-	# Grep the blast result and return the number of hits, put in dataframe
-	grepreturn <- grep('hits found', readLines(blastoutput), value=T)
-	df <- as.data.frame(grepreturn)
-	df2 <- separate(df, grepreturn, c("hash", "numhits", "hits", "found"), " ")
+  ## Grep the blast result and return the total number of hits, put in dataframe
+  grepreturn <- grep('hits found', readLines(blastoutput), value=T)
+  df <- as.data.frame(grepreturn)
+  df2 <- separate(df, grepreturn, c("hash", "numhits", "hits", "found"), " ")
 
-	numcandidates <- length(row.names(df2))
-	minhits <- min(as.numeric(df2$numhits))
-	maxhits <- max(as.numeric(df2$numhits))
+  # Divide the blast results into the different kmers in individual temporary files
+  dir.create("./TempFolder")
+  blastoutputlines <- readLines(blastoutput)
+  nlines <- length(blastoutputlines)
 
-	# Now, check all primer pair sets with left primer between 0 to 1/3 of non-target length and right primer between 2/3 to end of non-target length
-	#OnesixthL <- floor((1/6) * numcandidates)
-	OnethirdL <- floor((1/3) * numcandidates)
-	TwothirdL <- floor((2/3) * numcandidates)
-	#FiveSixthsL <- floor((5/6) * numcandidates)
+  queryline <- 0
+  for(i in 1:nlines){
 
-	primerpairs <- setNames(data.frame(matrix(ncol = 7, nrow = 0)), c("Left", "Right", "LeftSeq","RightSeq","HitsLeft", "HitsRight", "HitsTotal"))
+    if(grepl("Query", blastoutputlines[i])){
+      # Make a new file in the temp directory for each kmer
+      currentline <- blastoutputlines[i]
+      kmernumber <- strsplit(currentline, split=" ")[[1]][3]
+      queryline <- 0
+      outfilename <- paste("./TempFolder/",kmernumber,".txt", sep="")
+      file.create(outfilename)
+      next
+    }
 
-	for(j in 1: OnethirdL){
-		for(i in TwothirdL: numcandidates){
+    if(queryline == 1){
+      # Get the headers into the output file
+      currentline <- blastoutputlines[i]
+      csfields <- strsplit(currentline, ":")[[1]][2]
+      fields <- paste(strsplit(csfields, ", ")[[1]], collapse="\t")
+      write(fields,file=outfilename)
+      queryline = queryline + 1
+      next
+    }
 
-			newline <- c(j, i, 	as.character(kmers$seqs[which(kmers$kmers==paste(">kmer_",j,sep=""))]),as.character(kmers$seqs[which(kmers$kmers==paste(">kmer_",i,sep=""))]), as.numeric(df2$numhits)[j], as.numeric(df2$numhits)[i], as.numeric(df2$numhits)[j] + as.numeric(df2$numhits)[i])
-			primerpairs[nrow(primerpairs) + 1,] <- newline
+    if(queryline > 2){
+      # After skipping the 3rd line (hits found) add the rest of the lines for the query, except for the commented line bfore the next "Query" line
+      currentline <- blastoutputlines[i]
+      if(grepl("#", currentline)){
+        queryline = queryline + 1
+        next
+      }
+      write(currentline,file=outfilename,append=TRUE)
+      queryline = queryline + 1
+      next
+    }
+    queryline = queryline + 1
+  }
 
-		}
-	}
+  numcandidates <- length(row.names(kmers))
 
-	# Sort the primer pairs by the least total number of hits and make the output file
-	sortpps <- primerpairs[order(as.numeric(primerpairs$HitsTotal)),]
-	write.table(sortpps, file=outputfile, sep="\t", row.names=F)
+  # Now, get the stats for each potential oligo
+  allprimers <- setNames(data.frame(matrix(ncol = 5, nrow = 0)), c("kmer", "Seq","HitsTotal", "Hits >90% aligned length", "Hits alignd at 3' End"))
 
-	# Print a graph of the number of hits per candidate oligos
-	pdf(file= outputgraph)
-	plot(row.names(df2), df2$numhits, xlab = c("Candidate blocking oligo position on non-target sequence"), ylab=c("Number of hits to target database"))
-	dev.off()
+  for(i in 1:numcandidates){
+    sequence <- as.character(kmers$seqs[which(kmers$kmers==paste(">kmer_",i,sep=""))])
+    HitsTotal <- as.numeric(df2$numhits)[i]
+    maxalignlength <- 0.9 * nchar(sequence)
+
+    filename <- paste("./TempFolder/kmer_",i,".txt", sep="")
+    kmertable <- read.table(file=filename, header=T, sep="\t")
+    HitsThreePrime <- length(which(kmertable$q..end > (nchar(sequence) - 3)))
+
+    HitsIdenticalPositions <- length(which(kmertable$identical > maxalignlength))
+
+    newline <- c(i, sequence, HitsTotal, HitsIdenticalPositions, HitsThreePrime)
+    allprimers[nrow(allprimers) + 1,] <- newline
+  }
+  allprimers$kmer <- as.numeric(allprimers$kmer)
+  write.table(allprimers, file=outputfile, sep="\t", row.names=F)
+
+  # Print a graph with the 3 different hit counts per candidate oligos
+  OnethirdL <- floor((1/3) * numcandidates)
+  TwothirdL <- floor((2/3) * numcandidates)
+
+  a <- ggplot(allprimers, aes(x=as.numeric(kmer), y=as.numeric(HitsTotal))) + geom_point() + xlab("kmer") + ylab("Total Hits") + theme_classic() + geom_vline(xintercept = OnethirdL, linetype="dashed", color = "dodgerblue", size=1.5) + geom_vline(xintercept = TwothirdL, linetype="dashed", color = "coral2", size=1.5)
+  b <- ggplot(allprimers, aes(x=as.numeric(kmer), y=as.numeric(`Hits >90% aligned length`))) + geom_point() + xlab("kmer") + ylab("Hits > 90% primer length")  + theme_classic() + geom_vline(xintercept = OnethirdL, linetype="dashed", color = "dodgerblue", size=1.5) + geom_vline(xintercept = TwothirdL, linetype="dashed", color = "coral2", size=1.5)
+  c <- ggplot(allprimers, aes(x=as.numeric(kmer), y=as.numeric(`Hits alignd at 3' End`))) + geom_point()+ xlab("kmer") + ylab("Hits aligned at 3' End")  + theme_classic() + geom_vline(xintercept = OnethirdL, linetype="dashed", color = "dodgerblue", size=1.5) + geom_vline(xintercept = TwothirdL, linetype="dashed", color = "coral2", size=1.5)
+  pdf(file=outputgraph, height=10)
+  print(ggarrange(a+ rremove("xlab")+rremove("x.text"),b+ rremove("xlab")+rremove("x.text"),c, labels = c("A","B","C"), ncol=1, nrow=3))
+  dev.off()
+
+  # Remove the temporary directory:
+  unlink("./TempFolder", recursive=TRUE)
 
 }
